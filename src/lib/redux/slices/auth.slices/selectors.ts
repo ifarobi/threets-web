6801645a11713d7a8750d96fc559746e2f7b5996
Ilham Farobi <ilhamfarobi@gmail.com>
2023-07-12 12:31:50 +0700

    chore: remove unused code

--- src/lib/redux/slices/auth.slices/selectors.ts
+++ src/lib/redux/slices/auth.slices/selectors.ts
@@ -1,4 +1,3 @@
 import { ReduxState } from "../../store";
 
 export const selectUser = (state: ReduxState) => state.auth.user;
-export const selectSession = (state: ReduxState) => state.auth.session;
